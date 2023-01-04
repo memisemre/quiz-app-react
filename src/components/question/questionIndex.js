db656e143dd628e4d2ff089edca37c544da8f79a
Emre Memiş <113227617+memisemre@users.noreply.github.com>
2023-01-04 16:08:28 +0300

    Create Restart App Button

--- src/components/question/questionIndex.js
+++ src/components/question/questionIndex.js
@@ -37,7 +37,7 @@ class QuestionArea extends React.Component {
 				this.changeQuestion();
 				this.setState({invalidAnswers : this.state.invalidAnswers +1});
 			}
-		}, 1000);
+		}, 100);
 	}
 	render() {
 		const activeQuestion = Questions[this.state.questionIndex];
