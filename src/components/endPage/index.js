1238c6ca6ea5f4a54c15012c33eb6cb92c535f0c
softyak <rsat.memis@gmail.com>
2023-01-05 21:27:18 +0300

    some little refactor

--- src/components/endPage/index.js
+++ src/components/endPage/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import App from '../../App';
 import { number } from 'prop-types';
 
 class EndPage extends React.Component {
-  state = {
-    restartGame: false
+  restart = () => {
+    window.location.reload();
   };
+
   render() {
-    if (this.state.restartGame === false) {
-      const { trueAnswers, falseAnswers, invalidAnswers } = this.props;
-      return (
-        <div className='end-page-container'>
-          <div className='true'>True Answers : {trueAnswers}</div>
-          <div className='false'>False Answers : {falseAnswers} </div>
-          <div className='invalid'>Invalid Answers : {invalidAnswers} </div>
-          <button onClick={() => { this.setState({ restartGame: true }); }}>Restart Game</button>
-        </div>
-      );
-    }
-    else {
-      return <App />;
-    }
+    const { trueAnswers, falseAnswers, invalidAnswers } = this.props;
+
+    return (
+      <div className='end-page-container'>
+        <div className='true'>True Answers : {trueAnswers}</div>
+        <div className='false'>False Answers : {falseAnswers} </div>
+        <div className='invalid'>Invalid Answers : {invalidAnswers} </div>
+        <button onClick={this.restart}>Restart Game</button>
+      </div>
+    );
   }
 }
 
--- src/components/question/index.js
+++ src/components/question/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import EndPage from '../endPage';
-import Questions from './questions.json';
+import questionList from './questions.json';
 class QuestionArea extends React.Component {
-  state = {
-    questionIndex: 0,
-    trueAnswers: 0,
-    falseAnswers: 0,
-    timerValue: 0,
-    invalidAnswers : 0
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionIndex: 0,
+      trueAnswers: 0,
+      falseAnswers: 0,
+      timerValue: 0,
+      invalidAnswers : 0
+    };
+    this.interval = null;
+  }
+
+  componentDidMount() {
+    this.timer();
+  }
+
   checkAnswer = (answerOption, trueAnswer) => {
     answerOption === trueAnswer ? (
       this.setState({ trueAnswers: this.state.trueAnswers + 1 })
@@ -16,58 +25,66 @@ class QuestionArea extends React.Component {
       this.setState({ falseAnswers: this.state.falseAnswers + 1 })
     );
   };
+
   changeQuestion = () => {
-    if(this.state.questionIndex !== Questions.length){
+    if (this.state.questionIndex !== questionList.length) {
       this.setState({ questionIndex: this.state.questionIndex + 1 });
       this.timer();
     }
   };
-  componentDidMount() {
-    this.timer();
-  }
+
+  handleTimeEnd = () => {
+    this.setState({ invalidAnswers: this.state.invalidAnswers + 1 });
+    this.changeQuestion();
+  };
+
   timer = () => {
     clearInterval(this.interval);
+
     this.setState({ timerValue: 5 });
+
     this.interval = setInterval(() => {
-      this.setState({
-        timerValue : this.state.timerValue -1
-      });
+      this.setState({ timerValue: this.state.timerValue - 1 });
+
       if (this.state.timerValue === 0) {
-        clearInterval(this.interval);
-        this.changeQuestion();
-        this.setState({invalidAnswers : this.state.invalidAnswers +1});
+        this.handleTimeEnd();
       }
     }, 1000);
   };
+
   render() {
-    const activeQuestion = Questions[this.state.questionIndex];
-    if (this.state.questionIndex === Questions.length){
+    const activeQuestion = questionList[this.state.questionIndex];
+    const isQuizEnd = this.state.questionIndex === questionList.length;
+
+    if (isQuizEnd){
       clearInterval(this.interval);
-      return <EndPage
-        trueAnswers={this.state.trueAnswers}
-        falseAnswers={this.state.falseAnswers}
-        invalidAnswers = {this.state.invalidAnswers}
-      />;
-    }
-    else {
+
       return (
-        <div className='question-container'>
-          <h1>{activeQuestion.qeustion}</h1>
-          <div className='answers-container'>
-            {activeQuestion.answerOptions.map(answerOption =>
-              <button
-                onClick={()=>{
-                  this.checkAnswer(answerOption, activeQuestion.trueAnswer);
-                  this.changeQuestion();
-                }}
-                key={answerOption}>
-                {answerOption}
-              </button>)}
-          </div>
-          <h1> {this.state.timerValue} </h1>
-        </div>
+        <EndPage
+          trueAnswers={this.state.trueAnswers}
+          falseAnswers={this.state.falseAnswers}
+          invalidAnswers={this.state.invalidAnswers}
+        />
       );
     }
+
+    return (
+      <div className='question-container'>
+        <h1>{activeQuestion.qeustion}</h1>
+        <div className='answers-container'>
+          {activeQuestion.answerOptions.map(answerOption =>
+            <button
+              onClick={()=>{
+                this.checkAnswer(answerOption, activeQuestion.trueAnswer);
+                this.changeQuestion();
+              }}
+              key={answerOption}>
+              {answerOption}
+            </button>)}
+        </div>
+        <h1> {this.state.timerValue} </h1>
+      </div>
+    );
   }
 }
 export default QuestionArea;
